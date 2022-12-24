4f834492f881c3ee0770a1fe12c698bb0fc8558d
krampos <vezlivcevnikita@gmail.com>
2022-12-24 23:34:22 +0300

    [2]feature: Added update method to telegram bot.

--- src/lib.rs
+++ src/lib.rs
@@ -7,29 +7,61 @@ pub struct Message {
     text: String,
 }
 
-struct TelegramBot {
+pub struct TelegramBot {
     api_token: String,
     telegram_api_url: String,
+    telegram_bot_api_url: String,
 }
 
 impl TelegramBot {
-    fn new(api_token: String) -> Self {
+    pub fn new(api_token: String) -> Self {
+        let token = String::from(&api_token);
         return TelegramBot {
             telegram_api_url: String::from("https://api.telegram.org/"),
             api_token,
+            telegram_bot_api_url: format!(
+                "{}{}{}{}",
+                "https://api.telegram.org/", "bot", token, "/"
+            ),
         };
     }
 
-    async fn send_message(&self, message: Message) -> Result<Response, Error> {
+    pub async fn get_me(&self) {
         let client = reqwest::Client::new();
         let res = client
-            .post(format!(
-                "{}{}{}{}",
-                self.telegram_api_url, "bot", &self.api_token, "/sendMessage"
-            ))
+            .get(format!("{}{}", self.telegram_bot_api_url, "getMe"))
+            .send()
+            .await;
+        println!("connect result: {}", res.unwrap().text().await.unwrap())
+    }
+
+    pub async fn send_message(&self, message: Message) -> Result<Response, Error> {
+        let client = reqwest::Client::new();
+        let res = client
+            .post(format!("{}{}", self.telegram_bot_api_url, "sendMessage"))
             .json(&message)
             .send()
             .await;
         return res;
+    }
+
+    pub async fn get_updates<T>(
+        &self,
+        callback: fn(&T),
+        update_timeout_millis: u64,
+        func_param: T,
+    ) {
+        let client = reqwest::Client::new();
+        loop {
+            let res = client
+            .get(format!(
+                "{}{}",
+                self.telegram_bot_api_url, "getUpdates"
+            ))
+            .send()
+            .await;
+            callback(&func_param);
+            thread::sleep(time::Duration::from_millis(update_timeout_millis));
+        }
     }
 }
