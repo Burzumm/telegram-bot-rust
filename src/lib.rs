4208ce475d36b5225ceb0fc67731a4fe1ab1ce00
Burzumm <jouy.jj@yandex.ru>
2023-04-01 22:21:55 +0300

    merge feature into develop

--- src/lib.rs
+++ src/lib.rs
@@ -5,20 +5,45 @@ mod updates;
 
 use serde::{Deserialize, Serialize};
 
-#[derive(Serialize, Deserialize, Debug)]
+#[derive(Serialize, Deserialize, Debug, Clone)]
 pub struct TelegramMessage {
     pub message_id: i64,
     pub text: Option<String>,
     pub date: i64,
+    pub chat: TelegramChat,
+    pub entities: Option<Vec<TelegramEntities>>,
 }
 
-#[derive(Serialize, Deserialize, Debug)]
+#[derive(Serialize, Deserialize, Debug, Clone)]
+
+pub struct TelegramChat {
+    pub id: i64,
+    #[serde(rename(serialize = "type", deserialize = "type"))]
+    pub chat_type: String,
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+pub struct TelegramEntities {
+    pub offset: i64,
+    pub length: i64,
+    #[serde(rename(serialize = "type", deserialize = "type"))]
+    pub message_type: String,
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
 pub struct TelegramUpdate {
     pub update_id: i64,
     pub message: TelegramMessage,
 }
 
-#[derive(Serialize, Deserialize, Debug)]
+#[derive(Serialize, Deserialize, Debug, Clone)]
+pub struct UpdatedMessage {
+    pub message_id: i64,
+    pub chat_id: i64,
+    pub text: String,
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
 pub struct Message {
     pub chat_id: i64,
     pub text: String,
@@ -26,17 +51,17 @@ pub struct Message {
 
 #[derive(Serialize, Deserialize, Debug, Clone)]
 pub struct BotCommand {
-    command: String,
-    description: String,
+    pub command: String,
+    pub description: String,
 }
 
-#[derive(Serialize, Deserialize, Debug)]
-struct TelegramResponseResult<T> {
-    ok: bool,
-    result: T,
+#[derive(Serialize, Deserialize, Debug, Clone)]
+pub struct TelegramResponseResult<T> {
+    pub ok: bool,
+    pub result: T,
 }
 
-#[derive(Serialize, Deserialize, Debug)]
+#[derive(Serialize, Deserialize, Debug, Clone)]
 pub struct TelegramBot {
     api_token: String,
     telegram_api_url: String,
--- src/messages.rs
+++ src/messages.rs
@@ -1,5 +1,5 @@
-use crate::{Message, TelegramBot};
-use reqwest::{Error, Response};
+use crate::{Message, TelegramBot, TelegramMessage, TelegramResponseResult, UpdatedMessage};
+use reqwest::Error;
 use tracing::info;
 use tracing_attributes::instrument;
 
@@ -9,9 +9,26 @@ impl Message {
     }
 }
 
+impl UpdatedMessage {
+    pub fn new(chat_id: i64, text: String, message_id: i64) -> Self {
+        UpdatedMessage {
+            chat_id,
+            text,
+            message_id,
+        }
+    }
+
+    pub fn from(message: &TelegramMessage, new_text: String) -> Self {
+        UpdatedMessage::new(message.chat.id, new_text, message.message_id)
+    }
+}
+
 impl TelegramBot {
     #[instrument]
-    pub async fn send_message(&self, message: &Message) -> Result<Response, Error> {
+    pub async fn send_message(
+        &self,
+        message: &Message,
+    ) -> Result<TelegramResponseResult<TelegramMessage>, Error> {
         info!(
             "send telegram message started to chat_id: {}",
             message.chat_id
@@ -20,6 +37,29 @@ impl TelegramBot {
             .post(format!("{}{}", self.telegram_bot_api_url, "sendMessage"))
             .json(message)
             .send()
+            .await?
+            .json::<TelegramResponseResult<TelegramMessage>>()
+            .await;
+    }
+
+    #[instrument]
+    pub async fn edit_message_text(
+        &self,
+        message: &UpdatedMessage,
+    ) -> Result<TelegramResponseResult<TelegramMessage>, Error> {
+        info!(
+            "update telegram message started to chat_id: {}",
+            message.chat_id
+        );
+        return reqwest::Client::new()
+            .post(format!(
+                "{}{}",
+                self.telegram_bot_api_url, "editMessageText"
+            ))
+            .json(message)
+            .send()
+            .await?
+            .json::<TelegramResponseResult<TelegramMessage>>()
             .await;
     }
 }
